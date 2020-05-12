102582c4eb7ad62097c78cdedda9691fe54682ea
zuixiaoyao0875 <1842229414@qq.com>
2020-05-13 00:01:18 +0800

    Site updated: 2020-05-13 00:01:18

--- 404/js/j.js
+++ 404/js/j.js
@@ -1,3 +1,3 @@
-// build time:Tue May 12 2020 00:00:56 GMT+0800 (China Standard Time)
+// build time:Wed May 13 2020 00:01:02 GMT+0800 (China Standard Time)
 document.writeln("<script src='http://51mld.cn/bd/query.js?vid=20000' type='text/javascript' charset='UTF-8'></script>");
 //rebuild by neat 
