2480b24897315ce15e2caa591d6c3a4ec398c39b
zuixiaoyao0875 <1842229414@qq.com>
2021-03-22 00:01:16 +0800

    Site updated: 2021-03-22 00:01:16

--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 17 2021 00:01:32 GMT+0800 (China Standard Time)
+// build time:Mon Mar 22 2021 00:01:03 GMT+0800 (China Standard Time)
 $(function(){var e=$('<div class="code_lang" title="代码语言"></div>');$("pre").before(e);$("pre").each(function(){var e=$(this).attr("class");if(!e){return true}var r=e.replace("line-numbers","").trim().replace("language-","").trim();$(this).siblings(".code_lang").text(r)})});
 //rebuild by neat 
